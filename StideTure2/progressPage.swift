c50a18c867bddb3650d852f8ef7e664939ccb960
elybellaluna <elysaldana05@gmail.com>
2025-07-09 11:30:44 -0500

    commit progress

--- StideTure2/progressPage.swift
+++ StideTure2/progressPage.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct progressPage: View {
     var body: some View {
-        Text("Hello, World!6")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
