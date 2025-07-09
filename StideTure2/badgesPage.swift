b3f90d020da996f164840057eef84dd10e504930
elybellaluna <elysaldana05@gmail.com>
2025-07-09 11:37:44 -0500

    commit test 2

---
+++ StideTure2/badgesPage.swift
@@ -0,0 +1,18 @@
+//
+//  badgesPage.swift
+//  StideTure2
+//
+//  Created by 52GOParticipant on 7/9/25.
+//
+
+import SwiftUI
+
+struct badgesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    badgesPage()
+}
