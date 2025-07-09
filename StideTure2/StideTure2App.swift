e10ef81b6364d2b9aac9269e668bee64a5d198de
elybellaluna <elysaldana05@gmail.com>
2025-07-09 11:35:52 -0500

    Reapply "Initial Commit"
    This reverts commit 1d29712ebc3b7437e28fc9983bd470c68b92780a.

---
+++ StideTure2/StideTure2App.swift
@@ -0,0 +1,17 @@
+//
+//  StideTure2App.swift
+//  StideTure2
+//
+//  Created by 52GOParticipant on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct StideTure2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
