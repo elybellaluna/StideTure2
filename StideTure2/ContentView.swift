7b0cd213c2a644706d4019bedc23642d515e2267
elybellaluna <elysaldana05@gmail.com>
2025-07-10 14:02:27 -0500

    purple start button

--- StideTure2/ContentView.swift
+++ StideTure2/ContentView.swift
@@ -10,13 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button("Start Stride"){
-        
+            Button("Start Stride") {
+                
             }
+            .padding(.all)
+            .frame(width: 300.0)
+            .background(Color(red: 0.5137254901960784, green: 0.40784313725490196, blue: 0.7254901960784313))
+            .foregroundColor(.white)
+            .cornerRadius(25.0)
         }
-        .padding()
+            
+            
+            
+            
+        }
+        
     }
-}
 
 #Preview {
     ContentView()
