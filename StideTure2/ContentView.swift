319e7a50cf29e3ecc3bdbeeddb001baa9946fa4b
elybellaluna <elysaldana05@gmail.com>
2025-07-11 09:36:52 -0500

    z stack added

--- StideTure2/ContentView.swift
+++ StideTure2/ContentView.swift
@@ -9,20 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-//Welcome back top screen
-            Text("Welcome Back")
-                .font(.system(size: 30, weight: .bold, design: .default))
-                .foregroundColor(Color(red: 0.1568627450980392, green: 0.47058823529411764, blue: 0.5254901960784314))
-                .frame(width: 300.0, height: 50.0)
-                .position(x:120, y:10)
-                .padding()
-//fetch user name for welcome back text (currently static)
-            var userName: String = "John"
-            Text("\(userName)!")
-                .font(.system(size: 50, weight: .bold, design: .default))
-                .foregroundColor(Color(red: 0.5137254901960784, green: 0.40784313725490196, blue: 0.7254901960784313))
-                .position(x:100, y:-300)
+        ZStack{
+            VStack {
+                //Welcome back top screen
+                Text("Welcome Back")
+                    .font(.system(size: 30, weight: .bold, design: .default))
+                    .foregroundColor(Color(red: 0.1568627450980392, green: 0.47058823529411764, blue: 0.5254901960784314))
+                    .position(x:120, y:10)
+                    .padding(.bottom, 20)
+                //fetch user name for welcome back text (currently static)
+                var userName: String = "John"
+                Text("\(userName)!")
+                    .font(.system(size: 50, weight: .bold, design: .default))
+                    .foregroundColor(Color(red: 0.5137254901960784, green: 0.40784313725490196, blue: 0.7254901960784313))
+                    .position(x:100, y:-300)
+            }
 //start stride button
             Button("Start Stride") {
                 
@@ -32,6 +33,7 @@ struct ContentView: View {
             .background(Color(red: 0.5137254901960784, green: 0.40784313725490196, blue: 0.7254901960784313))
             .foregroundColor(.white)
             .cornerRadius(25.0)
+            
         }
             
        
