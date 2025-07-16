adf734a6d371ee984cbaec31aa0917957722a6d8
elybellaluna <elysaldana05@gmail.com>
2025-07-16 12:05:02 -0500

    slideshow images

---
+++ StideTure2/objectSlideshow.swift
@@ -0,0 +1,18 @@
+//
+//  objectSlideshow.swift
+//  StideTure2
+//
+//  Created by 52GOParticipant on 7/16/25.
+//
+
+import SwiftUI
+
+struct objectSlideshow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    objectSlideshow()
+}
