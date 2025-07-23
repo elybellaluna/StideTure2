e3391df306cab4fd22cfb03ad4f9fa914066b51e
briggite12 <briggite1204@gmail.com>
2025-07-23 12:12:32 -0500

    idk

---
+++ StideTure2/DistanceMapView.swift
@@ -0,0 +1,18 @@
+//
+//  DistanceMapView.swift
+//  StideTure2
+//
+//  Created by 54GOParticipant on 7/23/25.
+//
+
+import SwiftUI
+
+struct DistanceMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DistanceMapView()
+}
