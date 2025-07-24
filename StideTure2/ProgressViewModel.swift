e17b2ff3ff6d32aabe1b349a80baaea9b0408cc5
briggite12 <briggite1204@gmail.com>
2025-07-24 15:54:29 -0500

    update

---
+++ StideTure2/ProgressViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressViewModel.swift
+//  StideTure2
+//
+//  Created by 54GOParticipant on 7/24/25.
+//
+
+import SwiftUI
+
+struct ProgressViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressViewModel()
+}
